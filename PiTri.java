5b76ee3b32b09b1d78f0ae4761d3bbf71c1de67a
dimOk <dnikitenko@yandex.ru>
2024-08-14 11:34:28 +0300

    все переделал

--- PiTri.java
+++ PiTri.java
@@ -1,7 +1,7 @@
 public class PiTri {
 
     public static void main(String[] args) {
-        if(args == null || args.length == 0){
+         if(args == null || args.length == 0){
             System.out.println("Введите количество пифагоровых троек для вывода или введите три числа для проверки.");
             return;
         }
